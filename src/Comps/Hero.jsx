e0f39b97ed7a5bebc143498caa7586e9da20c29c
Rahul <rahulkrish321123@gmail.com>
2023-07-14 01:22:33 +0530

    Hero and nav Done

---
+++ src/Comps/Hero.jsx
@@ -0,0 +1,13 @@
+import "./hero.css"
+
+
+export default function Hero(){
+    return (
+        <div id={"hero_container"}>
+            <div className={"hero_text"}>
+                <p>UNLEASH YOUR HUMOR INTO </p>
+                <p>MEMES</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/Comps/nav.jsx
@@ -0,0 +1,12 @@
+
+import "./nav.css"
+export default function Nav() {
+    return(
+        <nav>
+            <div className={"navcontent"}>
+                <img alt={"."} src={"./assets/logo.png"} />
+                <p>MemeGenie</p>
+            </div>
+        </nav>
+    )
+} 
